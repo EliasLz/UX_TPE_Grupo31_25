f660311fe6d31adaa0e64eecb127bf6491e8453a
Ruso312 <marcodace00@gmail.com>
2025-09-30 16:46:13 -0300

    Add mock game data for Peg Solitaire: The Simpsons Edition

--- js/fetchApi.js
+++ js/fetchApi.js
@@ -1,4 +1,7 @@
-//Fetching de datos de la API
+import { mockGame } from "./mock.js";
+
+//Fetching de datos de la API"
+
 const API_URL = 'https://vj.interfaces.jima.com.ar/api/v2';
 
 export async function fetchGames() {
@@ -8,11 +11,13 @@ export async function fetchGames() {
             throw new Error('Error al obtener los juegos');
         }
 
-        const data = await response.json();
-        console.log(data)
+        let data = await response.json();
+        data = data.concat(mockGame); //--> Introducimos al Peg Solitarie en el array de juegos.
+
         return data;
     }catch(e){
         console.error('Hubo un error al obtener los juegos');
         console.error(e);
+        throw e;
     }
 }
