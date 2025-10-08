0ab706d1996184752a49d4ed5e569a5757f05cda
Hernan Gonzalez <hernangonzalez1373@gmail.com>
2025-10-07 22:41:38 -0300

    feat: Refactor header and footer structure, remove unused elements, and enhance menu toggle functionality

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,16 @@
 export function mainHamburguer(){
-        const menuToggle = document.getElementById('menuToggle'); // El ícono de la hamburguesa
-        const sideMenu = document.getElementById('sideMenu');     // El contenedor <aside class="menu">     
+        const menuToggle = document.getElementById('menuToggle');
+        const sideMenu = document.getElementById('sideMenu');     
 
         function toggleMenu() {
             sideMenu.classList.toggle('active');
         }       
-        // 3. Agregar el Event Listener al botón de hamburguesa
         menuToggle.addEventListener('click', (event) => {
-            // Evita que el clic se propague al documento inmediatamente
             event.stopPropagation(); 
             toggleMenu();
         });
         
         document.addEventListener('click', (event) => {
-            // Verifica si el menú está abierto Y si el clic NO fue dentro del menú Y el clic NO fue en el botón
             const isClickInsideMenu = sideMenu.contains(event.target);
             const isClickOnToggle = menuToggle.contains(event.target);
             
