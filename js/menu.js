08cd951c177a1b1373597fc548711053ac1a6e4c
Hernan Gonzalez <hernangonzalez1373@gmail.com>
2025-10-16 10:23:52 -0300

    feat: Refactor header structure and styles; implement user menu toggle functionality

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 export function mainHamburguer(){
         const menuToggle = document.getElementById('menuToggle');
         const sideMenu = document.getElementById('sideMenu');     
+        const userToggle = document.getElementById('userToggle');
+        const sideUser = document.getElementById('sideUser');
 
         function toggleMenu() {
             sideMenu.classList.toggle('active');
@@ -11,12 +13,27 @@ export function mainHamburguer(){
             toggleMenu();
         });
         
+        function toggleUser() {
+            sideUser.classList.toggle('active');
+        }
+
+        userToggle.addEventListener('click', (event) => {
+            event.stopPropagation(); 
+            toggleUser();
+        });
+        
         document.addEventListener('click', (event) => {
             const isClickInsideMenu = sideMenu.contains(event.target);
-            const isClickOnToggle = menuToggle.contains(event.target);
+            const isClickOnMenuToggle = menuToggle.contains(event.target);
+            const isClickOnUserToggle = userToggle.contains(event.target);
+            const isClickInsideUser = sideUser.contains(event.target);
             
-            if (sideMenu.classList.contains('active') && !isClickInsideMenu && !isClickOnToggle) {
+            if (sideMenu.classList.contains('active') && !isClickInsideMenu && !isClickOnMenuToggle) {
                 sideMenu.classList.remove('active');
+            } else {
+                if (sideUser.classList.contains('active') && !isClickInsideUser && !isClickOnUserToggle){
+                    sideUser.classList.remove('active');
+                }
             }
         });
 };
